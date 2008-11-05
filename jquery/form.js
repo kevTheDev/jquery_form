6c2bd94e0e5f96a71e45ad2fb60f8a6fcde13491
Kevin Edwards <kev@Macintosh-2.local>
2008-11-05 12:34:33 +0000

    added more packages

--- jquery/form.js
+++ jquery/form.js
@@ -1,17 +1,35 @@
 $(document).ready(function() { 
   
-	budget_checkbox = $("#budget_check");
-	premium_checkbox = $("#premium_check");
+	economy_check = $("#economy_check");
+	standard_check = $("#standard_check");
+	priority_check = $("#priority_check");
+	express_check = $("#express_check");
+	special_check = $("#special_check");
 	
 	total_cost = $("#iteration_total_cost");
 	
-	budget_checkbox.click(function(){
-		total_cost.text("10");		
+	
+	
+	
+	economy_check.click(function(){
+		total_cost.text("1000");		
 		
 	});
 	
-	premium_checkbox.click(function(){
-		total_cost.text("50");
+	standard_check.click(function(){
+		total_cost.text("1250");
+	});
+	
+	priority_check.click(function(){
+		total_cost.text("1500");
+	});
+	
+	express_check.click(function(){
+		total_cost.text("2000");
+	});
+	
+	special_check.click(function(){
+		total_cost.text("3000");
 	});
   
 });
